10aba0b531ec2f9e4840b7243391b14a658d6324
kingarj <alice.king@informed.com>
2025-04-03 16:38:11 +0100

    feat: [DPAV-701] uppercase TOID

--- address-profiling-pipeline/mapper/src/models/structure_unit.py
+++ address-profiling-pipeline/mapper/src/models/structure_unit.py
@@ -67,7 +67,7 @@ class StructureUnit:
         """
         uprn_uri = create_record_uri(record, "UPRN")
         first_line_of_address_uri = create_record_uri(record, "FirstLineOfAddress")
-        toid_uri = create_record_uri(record, "toid")
+        toid_uri = create_record_uri(record, "TOID")
         postcode_uri = f"{data_ns}PCODE_{record["Postcode"].replace(" ", "_")}"
         return [uprn_uri, first_line_of_address_uri, toid_uri, postcode_uri]
     
